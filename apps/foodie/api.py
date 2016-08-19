c8dbd74d287b17512ee5c546b854a37cfa691753
Alec Barlow <abarlow85@gmail.com>
2016-08-19 12:37:10 -0700

    initial commit

--- apps/foodie/api.py
+++ apps/foodie/api.py
@@ -1,5 +1,5 @@
 class Food2ForkAPI(object):
-	key = "d40b90968cc0a0052dd0155deb54550f"
+	key = ""
 	def search(self, term):
 		return "http://food2fork.com/api/search?key="+self.key+"&q="+term+"&sort=r&page=1"
 	def getOne(self, id):
--- food_registry/settings.py
+++ food_registry/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '($o!9x0qdlhi4eqjyc0fi^fd$=6-%3!)#40+5sts#belsfs9s!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
